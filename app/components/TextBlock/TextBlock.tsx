8fe8afe2c099269db6cb8a8fa95a196c711056ed
Christopher Mallon <christopher.mallon@gmail.com>
2023-01-23 08:43:40 -0500

    Second commit

--- app/components/TextBlock/TextBlock.tsx
+++ app/components/TextBlock/TextBlock.tsx
@@ -1,11 +1,11 @@
 type TextBlockProps = {};
 function TextBlock({}: TextBlockProps): JSX.Element {
 	return (
-		<div className={`flex flex-col`}>
-			<div id="title"></div>
-			<div id="body"></div>
-			<div id="icons"></div>
-			<div id="cta"></div>
+		<div className="flex flex-col">
+			<div id="title" />
+			<div id="body" />
+			<div id="icons" />
+			<div id="cta" />
 		</div>
 	);
 }
