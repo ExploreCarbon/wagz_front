838148a54d22cd318e7a62db17f4e17efe60509f
Christopher Mallon <christopher.mallon@gmail.com>
2023-02-07 15:41:10 -0500

    Deploy to Netlify

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -31,45 +31,54 @@ export default function Index() {
     switch (page.__typename) {
       case "Page_Pages_Content_HeroImage":
         return (
-          <Hero
-            largeScreenImage={page.image?.largeScreenImage?.mediaItemUrl}
-            mediumScreenImage={page.image?.largeScreenImage?.mediaItemUrl}
-            smallScreenImage={page.image?.largeScreenImage?.mediaItemUrl}
-            alt={page?.altText}
-            key={index}
-          >
-            <Container>
-              <div className={`homepage-hero w-full lg:w-3/5`}>
-                {page.contentBlock?.heading && (
-                  <Headline
-                    size="h1"
-                    extraClass={`title ${page.contentBlock?.heading?.color}  ${page.contentBlock?.heading?.size}`}
-                  >
-                    {page.contentBlock?.heading?.heading}
-                  </Headline>
-                )}
-                {page.contentBlock?.text && (
-                  <div className="text pt-8">
-                    <Text text={page.contentBlock?.text} />
-                  </div>
-                )}
-                <div className="flex flex-col justify-start gap-8 pt-8 lg:flex-row">
-                  {page.contentBlock?.cta?.cta &&
-                    page.contentBlock?.cta?.cta?.map((cta, index) => {
-                      return (
-                        <Link
-                          key={index}
-                          to={cta?.url}
-                          className={`button ${cta?.size} ${cta?.color}`}
-                        >
-                          {cta?.label}
-                        </Link>
-                      );
-                    })}
-                </div>
+          <div key={index} className="relative">
+            <div className="callout">
+              <div className="flex flex-col items-center justify-center gap-4 px-8 text-center">
+                <span className="title">Bundle & Save</span>
+                <span className="text">
+                  Find the option that best fits your lifestyle.
+                </span>
               </div>
-            </Container>
-          </Hero>
+            </div>
+            <Hero
+              largeScreenImage={page.image?.largeScreenImage?.mediaItemUrl}
+              mediumScreenImage={page.image?.largeScreenImage?.mediaItemUrl}
+              smallScreenImage={page.image?.largeScreenImage?.mediaItemUrl}
+              alt={page?.altText}
+            >
+              <Container>
+                <div className={`homepage-hero w-full md:w-1/2 xl:w-3/5`}>
+                  {page.contentBlock?.heading && (
+                    <Headline
+                      size="h1"
+                      extraClass={`title ${page.contentBlock?.heading?.color}  ${page.contentBlock?.heading?.size}`}
+                    >
+                      {page.contentBlock?.heading?.heading}
+                    </Headline>
+                  )}
+                  {page.contentBlock?.text && (
+                    <div className="text pt-8">
+                      <Text text={page.contentBlock?.text} />
+                    </div>
+                  )}
+                  <div className="flex flex-col justify-start gap-8 pt-8 lg:flex-row">
+                    {page.contentBlock?.cta?.cta &&
+                      page.contentBlock?.cta?.cta?.map((cta, index) => {
+                        return (
+                          <Link
+                            key={index}
+                            to={cta?.url}
+                            className={`button ${cta?.size} ${cta?.color}`}
+                          >
+                            {cta?.label}
+                          </Link>
+                        );
+                      })}
+                  </div>
+                </div>
+              </Container>
+            </Hero>
+          </div>
         );
       case "Page_Pages_Content_Icons":
         return (
